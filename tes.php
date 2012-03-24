41feb1c9aa563901e60ad8c3af92e9c511ca58cb
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-03-24 19:01:27 +0700

    install mysql and php5-mysql

--- tes.php
+++ tes.php
@@ -1,10 +1,17 @@
 #!/usr/bin/php -q
 <?php
 
-echo "Free Space in / : ";
-echo disk_free_space("/");
-echo " Byte\n";
-echo "Free Space in /home : ";
-echo disk_free_space("/home");
-echo " Byte\n";
+$con = mysql_connect("localhost","root","slamdunk");
 
+if(!$con) {
+	die('Could not connect : '.mysql_error());
+}
+else {
+
+	echo "Free Space in / : ";
+	echo disk_free_space("/");
+	echo " Byte\n";
+	echo "Free Space in /home : ";
+	echo disk_free_space("/home");
+	echo " Byte\n";
+}
