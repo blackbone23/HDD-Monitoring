f5d01a97a7291fd69629fe1e52017a9e34152ff6
Skynet <skynet@ubuntu.ubuntu-domain>
2012-03-25 17:25:01 +0700

    make it Gigabyte file

--- tes.php
+++ tes.php
@@ -1,17 +1,22 @@
 #!/usr/bin/php -q
 <?php
 
-$con = mysql_connect("localhost","root","slamdunk");
+$con = mysql_connect("localhost","root","kartika");
 
 if(!$con) {
 	die('Could not connect : '.mysql_error());
 }
 else {
-
+	$root = disk_free_space("/");
+	$home = disk_free_space("/home");
+	$root_giga = (($root/1024)/1024)/1024;
+	$home_giga = (($home/1024)/1024)/1024;
+	preg_match("/(\d{2,3})(.\.*)(\d{2})/is",$root_giga,$matches);
+	preg_match("/(\d{2,3})(.\.*)(\d{2})/is",$root_giga,$matches2);	
 	echo "Free Space in / : ";
-	echo disk_free_space("/");
-	echo " Byte\n";
+	echo $matches[0];
+	echo " GB\n";
 	echo "Free Space in /home : ";
-	echo disk_free_space("/home");
-	echo " Byte\n";
+	echo $matches2[0];
+	echo " GB\n";
 }
