88460a46294f289dc4eef4d259b044a489ae71ce
M Khairul Lukman W <rully@bippo.co.id>
2012-04-03 17:24:01 +0700

    belum ketemu solusi ambil bagian dari listnya

--- tes.py
+++ tes.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
 import commands
+import re
 foo = commands.getoutput("df -h")
+foo_split = foo.split("\n")
+foo_split.pop(0)
+for string in foo_split:
+	string_split = string.split(" ")
+	print string_split
+	
+print len(foo_split)
 
-file = open("foo","a")
-file.write(foo)
-file.write("\n\n")
+
