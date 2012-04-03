718f8b9b7e5fc808024f3866e3bf660afd075950
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-04-03 22:13:09 +0700

    semoga regexnya benar

--- tes.py
+++ tes.py
@@ -3,11 +3,16 @@
 import commands
 import re
 foo = commands.getoutput("df -h")
+print foo
 foo_split = foo.split("\n")
 foo_split.pop(0)
 for string in foo_split:
 	string_split = string.split(" ")
-	print string_split
+#	print string
+	result = re.match("(.*?\s)\s{1,20}(0|.*?\s)\s{1,20}(0|.*?[A-Z])\s{1,20}(0\s|.*?[A-Z]\s)\s{1,20}(.*?%)\s{1,20}(.*)",string)
+	print result.groups()	
+
+#print result.groups()
 	
 print len(foo_split)
 
