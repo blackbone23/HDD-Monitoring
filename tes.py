ca9abb9646395f56b6a5ca4ae30ebf0a53602995
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-04-04 19:52:47 +0700

    sudah memenuhi struktur df

--- tes.py
+++ tes.py
@@ -9,7 +9,8 @@ foo_split.pop(0)
 for string in foo_split:
 	string_split = string.split(" ")
 	#print string_split
-	result = re.match("(.*?\s)\s{1,20}(0|.*?\s)\s{1,20}(0|.*?[A-Z])\s{1,20}(0\s|.*?[A-Z]\s)\s{1,20}(.*?%)\s{1,20}(.*)",string)
+#	result = re.match("(.*?\s)\s{1,20}(0|.*?\s)\s{1,20}(0|.*?[A-Z])\s{1,20}(0\s|.*?[A-Z]\s)\s{1,20}(.*?%)\s{1,20}(.*)",string)
+	result = re.match("(.*?\s)\s{1,20}([0-9].*?[0-9])\s{1,20}(0|[0-9].*?[0-9])\s{1,20}(0|[0-9].*?[0-9])\s{1,20}(0.*?%|[0-9].*?%)\s{1,20}(\/.*)",string)
 	print result.groups()	
 
 #print result.groups()
