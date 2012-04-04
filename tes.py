f1755a986b524a5246ca1ed2975152df36a84ba1
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-04-04 21:53:15 +0700

    make any match to file system / or /home formated to array

--- tes.py
+++ tes.py
@@ -6,10 +6,13 @@ foo = commands.getoutput("df")
 print foo
 foo_split = foo.split("\n")
 foo_split.pop(0)
+a = []
 for string in foo_split:
 	string_split = string.split(" ")
 	result = re.match("(.*?\s)\s{1,20}([0-9].*?[0-9])\s{1,20}(0|[0-9].*?[0-9])\s{1,20}(0|[0-9].*?[0-9])\s{1,20}(0.*?%|[0-9].*?%)\s{1,20}(\/.*)",string)
 	if (result.group(6) == "/" or result.group(6) == "/home"):
-		print result.groups()
-
-
+		b=[]
+		for string2 in result.groups():
+			b.append(string2)
+		a.append(b)
+print a
