a0195be516a6845d42e38d5a2d938474615e7cb6
M Khairul Lukman W <rully@bippo.co.id>
2012-04-04 18:22:20 +0700

    harusnya pake df bukan df-h

--- tes.py
+++ tes.py
@@ -2,18 +2,18 @@
 
 import commands
 import re
-foo = commands.getoutput("df -h")
+foo = commands.getoutput("df")
 print foo
 foo_split = foo.split("\n")
 foo_split.pop(0)
 for string in foo_split:
 	string_split = string.split(" ")
-#	print string
+	#print string_split
 	result = re.match("(.*?\s)\s{1,20}(0|.*?\s)\s{1,20}(0|.*?[A-Z])\s{1,20}(0\s|.*?[A-Z]\s)\s{1,20}(.*?%)\s{1,20}(.*)",string)
 	print result.groups()	
 
 #print result.groups()
 	
-print len(foo_split)
+#print len(foo_split)
 
 
