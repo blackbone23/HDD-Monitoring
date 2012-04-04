80aef894ac1757ef99beeace49e246d4007159db
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-04-04 19:55:49 +0700

    removes code inside command

--- tes.py
+++ tes.py
@@ -8,14 +8,8 @@ foo_split = foo.split("\n")
 foo_split.pop(0)
 for string in foo_split:
 	string_split = string.split(" ")
-	#print string_split
-#	result = re.match("(.*?\s)\s{1,20}(0|.*?\s)\s{1,20}(0|.*?[A-Z])\s{1,20}(0\s|.*?[A-Z]\s)\s{1,20}(.*?%)\s{1,20}(.*)",string)
 	result = re.match("(.*?\s)\s{1,20}([0-9].*?[0-9])\s{1,20}(0|[0-9].*?[0-9])\s{1,20}(0|[0-9].*?[0-9])\s{1,20}(0.*?%|[0-9].*?%)\s{1,20}(\/.*)",string)
 	if (result.group(6) == "/" or result.group(6) == "/home"):
 		print result.groups()
-
-#print result.groups()
-	
-#print len(foo_split)
 
 
