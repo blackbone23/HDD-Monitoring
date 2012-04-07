75052f46a38f37723bbab7db7d30dea285572226
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-04-07 12:06:34 +0700

    renew db

--- hdd_monitor.sql
+++ hdd_monitor.sql
@@ -16,6 +16,62 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `hdd_device`
+--
+
+DROP TABLE IF EXISTS `hdd_device`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hdd_device` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `IP` varchar(45) DEFAULT NULL,
+  `device` varchar(45) DEFAULT NULL,
+  `filetype` varchar(45) DEFAULT NULL,
+  `mount_on` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hdd_device`
+--
+
+LOCK TABLES `hdd_device` WRITE;
+/*!40000 ALTER TABLE `hdd_device` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hdd_device` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hdd_status`
+--
+
+DROP TABLE IF EXISTS `hdd_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hdd_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `IP` varchar(45) DEFAULT NULL,
+  `device` varchar(45) DEFAULT NULL,
+  `filetype` varchar(45) DEFAULT NULL,
+  `mount_on` varchar(45) DEFAULT NULL,
+  `used` varchar(45) DEFAULT NULL,
+  `free` varchar(45) DEFAULT NULL,
+  `percent` varchar(45) DEFAULT NULL,
+  `total` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hdd_status`
+--
+
+LOCK TABLES `hdd_status` WRITE;
+/*!40000 ALTER TABLE `hdd_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hdd_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -50,4 +106,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-04-01 15:59:17
+-- Dump completed on 2012-04-07 12:06:19
