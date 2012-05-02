8027cccf1f8d0714d74163d615d3f613de0ff91e
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-05-03 00:06:21 +0700

    template utk display data di grafiknya, tp masih dicomment

--- hdd_monitor/application/views/view_charts.php
+++ hdd_monitor/application/views/view_charts.php
@@ -179,13 +179,13 @@
 		        type: 'line'
 		     },
 		     title: {
-		        text: 'HDD Space Consumption Month <?php echo switch_month($month) ?>'
+		        text: 'HDD Space Consumption Month <?php echo switch_month($month)." ".$year; ?>'
 		     },
 		     subtitle: {
 		        text: 'Source : http://<?php echo $IP ?>'
 		     },
 		     xAxis: {
-		        categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+		        categories: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9', 'Day 10', 'Day 11', 'Day 12', 'Day 13', 'Day 14', 'Day 15', 'Day 16', 'Day 17', 'Day 18', 'Day 19', 'Day 20', 'Day 21', 'Day 22', 'Day 23', 'Day 24', 'Day 25', 'Day 26', 'Day 27', 'Day 28', 'Day 29', 'Day 30', 'Day 31']
 		     },
 		     yAxis: {
 		        title: {
@@ -200,6 +200,7 @@
 		     tooltip: {
 		        crosshairs: true,
 		        shared: true
+			
 		     },
 		     plotOptions: {
 		        spline: {
@@ -208,7 +209,13 @@
 		                lineColor: '#666666',
 		                lineWidth: 1
 		            }
-		        }
+		        }//,
+			//line: {
+                    	//	dataLabels: {
+                        //		enabled: true
+                    	//	},
+                    	//	enableMouseTracking: true
+                	//}
 		     },
 		     series: [
 			<?php
