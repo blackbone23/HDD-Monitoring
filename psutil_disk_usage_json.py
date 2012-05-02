a022a8d731a88fd7bf601333525d82791fcfd89c
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-05-03 01:25:19 +0700

    psutil disk usage json akan digunakan untuk pengecekan data hardisk langsung via ssh, nantinya sebisa mungkin dijadikan opensource git

--- psutil_disk_usage_json.py
+++ psutil_disk_usage_json.py
@@ -3,6 +3,7 @@
 import psutil
 import json
 partitions = psutil.disk_partitions()
+json_arr=[]
 for partition in partitions :
   if (partition.mountpoint == "/" or partition.mountpoint == "/home"):
     device = partition.device
@@ -10,10 +11,12 @@ for partition in partitions :
     filesystype = partition.fstype
     if(mount == "/") :
       check_slash = psutil.disk_usage(mount)
-      check_slash_json = json.dumps({'mount on':mount, 'total':check_slash.total, 'used':check_slash.used, 'free':check_slash.free, 'percent':check_slash.percent, 'filetype':filesystype, 'device':device})
-      print check_slash_json
+      check_slash = {'mount on':mount, 'total':check_slash.total, 'used':check_slash.used, 'free':check_slash.free, 'percent':check_slash.percent, 'filetype':filesystype, 'device':device}
+      json_arr.append(check_slash)
     else :
       check_slash_home = psutil.disk_usage("/home")
-      check_slash_home_json = json.dumps({'mount on':mount, 'total':check_slash_home.total, 'used':check_slash_home.used, 'free':check_slash_home.free, 'percent':check_slash_home.percent,  'filetype':filesystype, 'device':device})
-      print check_slash_home_json
+      check_slash_home = {'mount on':mount, 'total':check_slash_home.total, 'used':check_slash_home.used, 'free':check_slash_home.free, 'percent':check_slash_home.percent,  'filetype':filesystype, 'device':device}
+      json_arr.append(check_slash_home)
+
+print json.dumps(json_arr)
 
