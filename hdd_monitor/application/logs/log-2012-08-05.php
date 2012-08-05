3fe7f914739dc4408acdeea4ce3ca3e903edd515
Muh Khairul Lukman W <rully.lukman@gmail.com>
2012-08-05 14:26:59 +0700

    file log

---
+++ hdd_monitor/application/logs/log-2012-08-05.php
@@ -0,0 +1,132 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2012-08-05 14:19:02 --> Config Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Hooks Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Utf8 Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> UTF-8 Support Enabled
+DEBUG - 2012-08-05 14:19:02 --> URI Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Router Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Output Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Security Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Input Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-08-05 14:19:02 --> Language Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Loader Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Helper loaded: url_helper
+DEBUG - 2012-08-05 14:19:02 --> Helper loaded: form_helper
+DEBUG - 2012-08-05 14:19:02 --> Helper loaded: html_helper
+DEBUG - 2012-08-05 14:19:02 --> Database Driver Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Form Validation Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Session Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Helper loaded: string_helper
+DEBUG - 2012-08-05 14:19:02 --> A session cookie was not found.
+DEBUG - 2012-08-05 14:19:02 --> Session routines successfully run
+DEBUG - 2012-08-05 14:19:02 --> Model Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Model Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Controller Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Model Class Initialized
+DEBUG - 2012-08-05 14:19:02 --> Helper loaded: file_helper
+DEBUG - 2012-08-05 14:19:02 --> Email Class Initialized
+ERROR - 2012-08-05 14:19:02 --> Severity: Notice  --> Undefined variable: row /home/rully/git/HDD-Monitoring/hdd_monitor/application/controllers/site.php 237
+ERROR - 2012-08-05 14:19:02 --> Severity: Notice  --> Trying to get property of non-object /home/rully/git/HDD-Monitoring/hdd_monitor/application/controllers/site.php 237
+DEBUG - 2012-08-05 14:19:09 --> Language file loaded: language/english/email_lang.php
+DEBUG - 2012-08-05 14:19:09 --> Your message has been successfully sent using the following protocol: mail<br /><pre>From: "Administrator Server" <tester.ryan212@gmail.com>
+Return-Path: <tester.ryan212@gmail.com>
+Reply-To: "tester.ryan212@gmail.com" <tester.ryan212@gmail.com>
+X-Sender: tester.ryan212@gmail.com
+X-Mailer: CodeIgniter
+X-Priority: 3 (Normal)
+Message-ID: <501e1e6613de1@gmail.com>
+Mime-Version: 1.0
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: 8bit
+=?utf-8?Q?Warning,_your_server_hard_disk_exceed_to_80%?=
+Hello ,
+
+I'm sorry for your inconvenience, but your hard disk resource is exceed to
+80% from total capacity. Here's result from our HDD Checker :
+
+IP : 192.168.11.31
+Partition : /dev/sda6
+Filetype : ext4
+Mount on : /
+Used : 6.099 GB
+Free : 3.134 GB
+Total : 9.727 GB
+Percent used : 62.7%
+
+
+Thank you for your attention.
+
+Regards,
+
+
+Administrator
+</pre>
+DEBUG - 2012-08-05 14:19:09 --> Final output sent to browser
+DEBUG - 2012-08-05 14:19:09 --> Total execution time: 7.2587
+DEBUG - 2012-08-05 14:20:49 --> Config Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Hooks Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Utf8 Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> UTF-8 Support Enabled
+DEBUG - 2012-08-05 14:20:49 --> URI Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Router Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Output Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Security Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Input Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Global POST and COOKIE data sanitized
+DEBUG - 2012-08-05 14:20:49 --> Language Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Loader Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Helper loaded: url_helper
+DEBUG - 2012-08-05 14:20:49 --> Helper loaded: form_helper
+DEBUG - 2012-08-05 14:20:49 --> Helper loaded: html_helper
+DEBUG - 2012-08-05 14:20:49 --> Database Driver Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Form Validation Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Session Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Helper loaded: string_helper
+DEBUG - 2012-08-05 14:20:49 --> A session cookie was not found.
+DEBUG - 2012-08-05 14:20:49 --> Session routines successfully run
+DEBUG - 2012-08-05 14:20:49 --> Model Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Model Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Controller Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Model Class Initialized
+DEBUG - 2012-08-05 14:20:49 --> Helper loaded: file_helper
+DEBUG - 2012-08-05 14:20:49 --> Email Class Initialized
+ERROR - 2012-08-05 14:20:49 --> Severity: Notice  --> Undefined variable: row /home/rully/git/HDD-Monitoring/hdd_monitor/application/controllers/site.php 237
+ERROR - 2012-08-05 14:20:49 --> Severity: Notice  --> Trying to get property of non-object /home/rully/git/HDD-Monitoring/hdd_monitor/application/controllers/site.php 237
+DEBUG - 2012-08-05 14:20:56 --> Language file loaded: language/english/email_lang.php
+DEBUG - 2012-08-05 14:20:56 --> Your message has been successfully sent using the following protocol: mail<br /><pre>From: "Your Computer" <tester.ryan212@gmail.com>
+Return-Path: <tester.ryan212@gmail.com>
+Reply-To: "tester.ryan212@gmail.com" <tester.ryan212@gmail.com>
+X-Sender: tester.ryan212@gmail.com
+X-Mailer: CodeIgniter
+X-Priority: 3 (Normal)
+Message-ID: <501e1ed1ca81c@gmail.com>
+Mime-Version: 1.0
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: 8bit
+=?utf-8?Q?Warning,_your_server_hard_disk_exceed_to_80%?=
+Hello ,
+
+I'm sorry for your inconvenience, but your hard disk resource is exceed to
+80% from total capacity. Here's result from our HDD Checker :
+
+IP : 192.168.11.31
+Partition : /dev/sda6
+Filetype : ext4
+Mount on : /
+Used : 6.099 GB
+Free : 3.134 GB
+Total : 9.727 GB
+Percent used : 62.7%
+
+
+Thank you for your attention.
+
+Regards,
+
+
+Administrator
+</pre>
+DEBUG - 2012-08-05 14:20:56 --> Final output sent to browser
+DEBUG - 2012-08-05 14:20:56 --> Total execution time: 6.2902
